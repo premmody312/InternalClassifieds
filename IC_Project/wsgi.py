0b583d5f46f37f8714b8b4128bae53f7d97bdac6
premmody312 <premmody312@gmail.com>
2021-07-15 12:54:49 +0530

    update

--- IC_Project/wsgi.py
+++ IC_Project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IC_Project3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IC_Project.settings')
 
 application = get_wsgi_application()
