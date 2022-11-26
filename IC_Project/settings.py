ae48b87f5e96f2fb13a5a5f061294c02e79961a0
Prem Mody <premmody312@gmail.com>
2022-11-26 12:07:19 +0530

    Update settings.py

--- IC_Project/settings.py
+++ IC_Project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o^vm8x54v5by)_#krfhkhehrz2&w4piy8ht*jazaq4$+6&z6u#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -126,7 +126,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'internalclassifiedstiaa@gmail.com'
-EMAIL_HOST_PASSWORD = 'Internal@123'
+EMAIL_HOST_PASSWORD = ''
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
